ff16a7986dc4841c6276d6b9bcb66cdb68f918e5
iggy228 <i.borovy@gmail.com>
2020-08-29 09:30:09 +0200

    Zotredenie kodu do funkcii v MainActivity a doladenie location baru v activity_main.xml

--- app/src/main/java/sk/example/skweather/MainActivity.kt
+++ app/src/main/java/sk/example/skweather/MainActivity.kt
@@ -6,6 +6,7 @@ import android.location.Location
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.EditText
 import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
@@ -26,8 +27,25 @@ import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var locationField: TextView
     private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+
+    private fun makesWeatherDays(city: String) {
+
+        val qVolley = Volley.newRequestQueue(this)
+        val url = "http://api.openweathermap.org/data/2.5/forecast?q=$city&lang=sk&units=metric&appid=acbd781e031d2ee5f734e15ee6641aad"
+
+
+        val jsonRequest = JsonObjectRequest(Request.Method.GET, url, null, Response.Listener {
+                response ->
+            for (i in 1..5) {
+                val fragment: WeatherDay = supportFragmentManager.findFragmentByTag("day$i") as WeatherDay
+                fragment.setViews(response, i)
+            }
+        }, Response.ErrorListener {
+
+        })
+        qVolley.add(jsonRequest)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -43,23 +61,9 @@ class MainActivity : AppCompatActivity() {
             location: Location? -> Log.d("mylog", "Longtitude is: ${location?.longitude} and altitude is: ${location?.altitude}");
         } */
 
-        locationField = findViewById(R.id.locationField)
-
-        val qVolley = Volley.newRequestQueue(this)
-        val url = "http://api.openweathermap.org/data/2.5/forecast?q=Rab%C4%8Dice&lang=sk&units=metric&appid=acbd781e031d2ee5f734e15ee6641aad"
-
-
-        val jsonRequest = JsonObjectRequest(Request.Method.GET, url, null, Response.Listener {
-            response -> locationField.text = response.getJSONObject("city").getString("name")
-
-            for (i in 1..5) {
-                val fragment: WeatherDay = supportFragmentManager.findFragmentByTag("day$i") as WeatherDay
-                fragment.setViews(response, i)
-            }
-        }, Response.ErrorListener {
-            locationField.text = "Ow something is wrong! ;("
-        })
-        qVolley.add(jsonRequest)
-
+    }
+    fun searchCity(view: View) {
+        val city = findViewById<EditText>(R.id.locationField).text.toString()
+        makesWeatherDays(city)
     }
 }
